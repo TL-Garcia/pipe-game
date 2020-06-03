066c14d60131f862901814389cd6187688ed2524
TL Garcia <tomas.bl.garcia@gmail.com>
2020-06-03 21:24:03 +0200

    Implemented rotation. Changed nomenclature of grid system. Could do it with IDs instead of classes but works so far. Could be interesting to automatize input of pipes on html.

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const pipes = [...document.querySelectorAll('.pipe')];
+
+
+const pipe1 = new Pipe(1, 1, 0);
+
--- src/pipe.js
+++ src/pipe.js
@@ -9,15 +9,28 @@ class Pipe {
             e: false
         };
 
+        this.element = document.querySelector(`.square.x${this.x}y${this.y} div`)
+
         /*Would be nice to limit input angle to 0-270*/
         this.angle = angle;
 
         this.active = false;
+
+        this._setClick();
+    }
+
+    _setClick() {
+        this.element.addEventListener('click', () => {
+            this.rotate();
+            console.log('rotating!')
+        })
     }
 
     rotate() {
         this.angle += 90;
         if (this.angle === 360) this.angle = 0;
+
+        this.element.style.transform = `rotate(${this.angle}deg)`;
     }
 
     _isActive() {
