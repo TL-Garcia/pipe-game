48ad3b22bd34ebd9842d692663f1d32bee7e2d6f
TL Garcia <tomas.bl.garcia@gmail.com>
2020-06-04 21:41:02 +0200

    Modified level implementation. Currently the plan is to model the grid as an array containing 6 subarrays which will represent the rows. This way each pipe's position is given by it's array index, so we can evaluate game conditions more easily.

--- src/level.js
+++ src/level.js
@@ -1,5 +1,16 @@
 class Level {
     constructor() {
+        this.grid = [
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+        ]
+    }
 
+    addPipe(row, x, y, angle) {
+        this.grid[row].push(new Pipe(x, y, angle))
     }
 }
