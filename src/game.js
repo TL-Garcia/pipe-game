d15ad5e95efeb763600fb4ef44cafab188b2021a
TL Garcia <tomas.bl.garcia@gmail.com>
2020-06-08 09:53:19 +0200

    isActive successfully checks if click target is being activated by another pipe. However, it doesn't de-activate it when it is no longer connected.
    A solution could be an algorithm which pushes every pipe from beginning
    to end in order to an array, and if a pipe isn't connected to the pipe
    before it, it would be disconnected along with the pipes after it.

--- src/game.js
+++ src/game.js
@@ -1,20 +1,20 @@
 class Game {
-
-
-    startLevel1() {
-        const level1 = new Level();
-
-        level1.addPipe(0, 0, 90)
-        level1.addPipe(0, 1, 0)
-        level1.addCurve(1, 1, 90)
-        level1.addCurve(1, 2, 270)
-        level1.addT(2, 2, 0)
-        level1.addPipe(3, 2, 0)
-        level1.addPipe(4, 2, 0)
-        level1.addPipe(5, 2, 0)
+    constructor() {
+        let currentLevel;
     }
 
-    _isActive(pipe) {
-        if (pipe.x === 0 && pipe.y === 0) return true;
+    startLevel1() {
+        this.currentLevel = new Level();
+
+        this.currentLevel.addPipe(0, 0, 90)
+        this.currentLevel.addCurve(0, 1, 0)
+        this.currentLevel.addCurve(1, 1, 90)
+        this.currentLevel.addCurve(1, 2, 270)
+        this.currentLevel.addT(2, 2, 0)
+        this.currentLevel.addPipe(3, 2, 0)
+        this.currentLevel.addPipe(4, 2, 0)
+        this.currentLevel.addPipe(5, 2, 0)
+
+        this.currentLevel.setClick();
     }
 }
--- src/level.js
+++ src/level.js
@@ -7,7 +7,7 @@ class Level {
             [null, null, null, null, null, null],
             [null, null, null, null, null, null],
             [null, null, null, null, null, null],
-        ]
+        ];
     }
 
     addPipe(x, y, angle) {
@@ -20,5 +20,66 @@ class Level {
 
     addT(x, y, angle) {
         this.grid[y][x] = new TPipe(x, y, angle);
+    }
+
+
+    setClick() {
+        const unifiedGrid = [].concat.apply([], this.grid);
+        unifiedGrid.forEach(p => {
+            p && p.element.addEventListener('click', () => {
+                p.angle += 90;
+                p._rotate();
+                this.isActive(p);
+            });
+        });
+    }
+
+    isActive(target) {
+        if (target.x === 0 && target.y === 0) target.active = true;
+
+        if (target.y - 1 >= 0 && target.direction.n) this._checkNeigbours(target, 'n');
+
+        if (target.x + 1 <= 5 && target.direction.e) this._checkNeigbours(target, 'e');
+
+        if (target.y + 1 <= 5 && target.direction.s) this._checkNeigbours(target, 's');
+
+        if (target.x - 1 > 0 && target.direction.w) this._checkNeigbours(target, 'w');
+
+        if (target.active) console.log(`I'm on ${target.x} ${target.y} and I'm active`)
+    }
+
+    _checkNeigbours(target, checkDir) {
+        const targetDir = target.direction[checkDir];
+        let neighbour;
+        let neighbourDir;
+
+        switch (checkDir) {
+            case 'n':
+                neighbour = this.grid[target.y - 1][target.x] ;
+                if (!neighbour) return;
+                neighbourDir = neighbour.direction.s;
+                break;
+
+            case 'e':
+                neighbour = this.grid[target.y][target.x + 1] ;
+                if (!neighbour) return;
+                neighbourDir = neighbour.direction.w;
+                break;
+
+            case 's':
+                neighbour = this.grid[target.y + 1][target.x] ;
+                if (!neighbour) return;
+                neighbourDir = neighbour.direction.n;
+                break;
+
+            case 'w':
+                neighbour = this.grid[target.y][target.x - 1] ;
+                if (!neighbour) return;
+                neighbourDir = neighbour.direction.e;
+                break;
+        }
+
+
+        if (neighbour.active && neighbourDir) target.active = true;
     }
 }
