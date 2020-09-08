ddea483ef892bc9fb5567d4e0c7d34da6d96c814
TL Garcia <tomas.bl.garcia@gmail.com>
2020-09-08 15:19:35 +0200

    Checkpoint

--- src/doc.js
+++ src/doc.js
@@ -6,17 +6,12 @@ class Doc {
         this.h = this.ctx.height;
         this.w = this.ctx.width;
 
-        this.img = new Image(25, 25);
+        this.img = new Image(5, 5);
         this.img.src = './img/doc.png';
 
     }
 
     draw() {
-        this.ctx.drawImage(this.img, this.x, this.y);
-        /*     
-        this.ctx.beginPath();
-        this.ctx.rect(20, 20, 150, 100);
-        this.ctx.stroke(); 
-        */
+        this.ctx.drawImage(this.img, this.x, this.y, 100, 150);
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-const game = new Game();
-game.startLevel0();
+const theme = new Audio('./sounds/theme.mp3');
+const introBtn = document.getElementById('intro-button');
+const animation = document.getElementById('intro-animation');
 
+const menuBtn = document.getElementById('menu-button');
+const menu = document.getElementById('menu');
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = 'none';
+    const game = new Game();
+    game.startLevel0();
+})
+
+introBtn.addEventListener('click', () => {
+    theme.play();
+    animation.style.display = 'block';
+});
